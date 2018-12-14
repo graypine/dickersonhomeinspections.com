5986634187c0594eb0929b7c966d8d0d3caed351
Michael Graham <mikegrahamcode@gmail.com>
2018-12-13 20:19:16 -0800

    initial design

---
+++ dickersoninspections.js
@@ -0,0 +1,18 @@
+const Metalsmith = require('metalsmith')
+const less = require('metalsmith-lesser')
+const markdown = require('metalsmith-markdown')
+const serve = require('metalsmith-serve')
+
+Metalsmith(__dirname)
+  .clean(true)
+  .use(less({
+    copySource: false,
+    lessOptions: {
+      plugins: []
+    }
+  }))
+  .use(serve())
+  .build((err) => {
+    if (err) throw err
+    console.log('build finished')
+  })
